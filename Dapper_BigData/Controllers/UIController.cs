cb89e929cd8bed593ba5de90b8868801c6dcda52
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-01-27 22:30:29 +0300

    Add project files.

---
+++ Dapper_BigData/Controllers/UIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_BigData.Controllers
+{
+    public class UIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
